95d1335349aeeff9b41fb73c65aa95a6fb914432
Sanket <sanket.shivam@soprasteria.com>
2021-12-16 23:55:22 +0530

    Adding profile pic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ButtonModule } from "primeng/button";
+import { AvatarModule } from "primeng/avatar";
+import { BadgeModule } from "primeng/badge";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule, 
+    AvatarModule, 
+    ButtonModule, 
+    BadgeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
